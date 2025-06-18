17a6bc9623df1eda3415fa39a325ecaa3d27ebe7
kronisrsudl <kronisrsudl@gmail.com>
2025-06-18 19:54:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -97,12 +97,15 @@ function Header() {
 }
 
 function App() {
+
+  const location = useLocation();
+  
   return (
     <Router>
       <div className="d-flex flex-column min-vh-100">
         <Header />
         <main className="flex-fill px-3 pt-3 pb-5 w-100" style={{ maxWidth: '1980px', margin: '0 auto' }}>
-          <Hero />
+          {location.pathname === '/' && <Hero />}
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/copyResep" element={<ProtectedRoute><FormCopyResep /></ProtectedRoute>} />
